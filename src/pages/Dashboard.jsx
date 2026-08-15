bde903c6881636fd95c26d631205959de7c9428f
Kristina Pavlovska <141680987+pavlovskakristina@users.noreply.github.com>
2026-08-15 20:00:14 +0200

    feat: Dodanie folderu page (Home, Dashboard, ex), dodanie routes. Planowana zmiana na createBrowserRouter

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { Link, Outlet } from 'react-router-dom';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Outlet />
+        </div>
+    );
+}
+
+
+export default Dashboard;
---
+++ src/pages/example.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+function Example() {
+    return (
+        <div>
+            <h1>Example</h1>
+            <Outlet />
+        </div>
+    );
+}
+
+
+export default Example;
