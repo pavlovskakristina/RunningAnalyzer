0efd7a6dd2653de0f60862150bb57bfce6e49ea6
Kristina Pavlovska <141680987+pavlovskakristina@users.noreply.github.com>
2026-08-16 16:10:12 +0200

    feat: Dodano funkcję zapisania imienia w LocalStorage i wykorzystywania go w innych miejscach strony

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,23 @@ import Home from './pages/Home';
 
 
 function RootLayout() {
+  const userName = localStorage.getItem("userName") || "";
+
   return (
     <>
       {/* Navigation */}
       {/* Renderuje komponenty zdefiniowane w trasach jako dzieci tego layoutu */}
-      < nav className='Navigation' >
-        < Link to="/">Home</Link> | {" "}
-        < Link to="/dashboard" > Dashboard</Link > | {" "}
-        < Link to="/example" > Example</Link >
+      < nav className='Navigation' style={{ display: 'flex', gap: '15px', padding: '10px', borderBottom: '1px solid black' }}>
+        <div>
+          < Link to="/">Home</Link> | {" "}
+          < Link to="/dashboard" > Dashboard</Link > | {" "}
+          < Link to="/example" > Example</Link >
+        </div>
+
+        <div style={{ marginLeft: 'auto', fontWeight: 'bold' }}>
+          Hello, {userName}!
+        </div>
+
       </nav >
       <Outlet />
 
@@ -35,12 +44,12 @@ function RootLayout() {
 // PROCES REJESTRACJI (LocalStorage)
 function RequireRegistration({ children }) {
   const CompletedSetup = localStorage.getItem("CompletedSetup");
-  
+
   if (!CompletedSetup) {
     // Jeśli użytkownik nie ma wpisu w localStorage, przekieruj go na stronę powitalną
     return <Navigate to="/welcome" replace />;
   }
-  
+
   // Użytkownik ma wpis w localStorage
   return children;
 }
@@ -54,9 +63,9 @@ const router = createBrowserRouter(
 
       <Route path="/" element={
         <RequireRegistration>
-        <RootLayout />
-      </RequireRegistration>
-    }>
+          <RootLayout />
+        </RequireRegistration>
+      }>
 
         <Route index element={<Home />} />
         <Route path="dashboard" element={<Dashboard />} />
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,13 +1,16 @@
 import { Outlet, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function WelcomePage() {
     const navigate = useNavigate();
+    const [name, setName] = useState(""); // Zmienna do przechowywania imienia użytkownika
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log("Dane zapisane!"); 
-        localStorage.setItem("hasCompletedSetup", "true"); // Zapisujemy, że setup jest zakończony
+        console.log("Data saved");
+        localStorage.setItem("userName", name);
+        localStorage.setItem("CompletedSetup", "true"); // Zapisujemy, że setup jest zakończony
         navigate("/", { replace: true });  // Przekierowujemy użytkownika na stronę główną
     };
 
@@ -19,7 +22,13 @@ function WelcomePage() {
             <form onSubmit={handleSubmit}>
                 {/* FORMULARZ */}
                 <div>
-                    <label>Name: <input type="text" required /></label>
+                    <label>Name: <input
+                        type="text"
+                        value={name} // Łączymy input ze zmienna
+                        onChange={(e) => setName(e.target.value)} // Aktualizujemy stan przy każdym wciśnięciu klawisza
+                        required
+                        style={{ marginLeft: '10px' }} />
+                    </label>
                 </div>
                 <button type="submit" style={{ marginTop: '20px' }}>
                     Okay
