a7e041b58bcf6e45acf727702bb846db9921237e
Kristina Pavlovska <141680987+pavlovskakristina@users.noreply.github.com>
2026-08-16 14:03:22 +0200

    refactoring: changing BrowserRouter to the createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { 
+  createBrowserRouter, 
+  RouterProvider, 
+  createRoutesFromElements, 
+  Route, 
+  Link, 
+  Outlet 
+} from "react-router-dom";
 
-// import Dashboard from './pages/Dashboard';  // usuń funkcję 
+// import the pages
+import Dashboard from './pages/Dashboard';  
 import Example from './pages/example';
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
+import Home from './pages/Home';
 
 
-function Dashboard() {
+function RootLayout() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Outlet />
-    </div>
-  );
+    <>
+      {/* Navigation */}
+      {/* Renderuje komponenty zdefiniowane w trasach jako dzieci tego layoutu */}
+      < nav className='Navigation' >
+        < Link to="/">Home</Link> | {" "}
+        < Link to="/dashboard" > Dashboard</Link > | {" "}
+        < Link to="/example" > Example</Link >
+      </nav >
+      <Outlet />  
+      
+    </>
+  )
 }
 
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
+{/* Routes */}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="example" element={<Example />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <nav className='Navigation'>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/dashboard">Dashboard</Link> |{" "}
-        <Link to="/contact">Contact</Link> |{" "}
-        <Link to="/example">Example</Link>
-
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />}>
-        </Route>
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return <RouterProvider router={router} />;
 }
+
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Outlet />
+        </div>    
+    )
+}
+
+export default Home;
