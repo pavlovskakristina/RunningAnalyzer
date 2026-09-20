6a39078e1de53a7b9faf2df873611085dfe92aa1
Kristina Pavlovska <141680987+pavlovskakristina@users.noreply.github.com>
2026-09-20 22:50:15 +0200

    chore(vite): configure dev server host and port 3000

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows to open the server on the local network (same WiFi network)
+    port: 3000,
+    strictPort: true, // allows only port 3000
+  },
 })
